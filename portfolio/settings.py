b89b0d22f4c782f043ea64f12b23be87e83309da
Debasish Nandi <debasishfairytail@gmail.com>
2020-05-16 11:59:52 +0530

    PORTFOLIO 3: HOME WEB PAGE made, bootstrap from getbootstrap included , page rendered , jobs given to homepage, objects from models learnt

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['example', 'localhost']
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
     'jobs.apps.JobsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
