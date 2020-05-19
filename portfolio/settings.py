42955a6477a877e7e77bd581be046f6b6b4b3cfe
Debasish Nandi <debasishfairytail@gmail.com>
2020-05-19 23:44:17 +0530

    PORTFOLIO 3 : added some static files did some changes to the boot strap version .... Manupulated the css , created bootstrap class new wall paper added (blur), {% load static %} learnt

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -125,6 +125,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
+STATICFILES_DIRS = [
+os.path.join(BASE_DIR,'portfolio/static'),
+]
+
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
@@ -134,4 +138,4 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static')
 # Media Files (images, videos,files etc.)
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
